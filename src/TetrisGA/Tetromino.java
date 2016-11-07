5f0cc9bb1df59e09d3cad756b0eec3ceee4b5b57
Gwozdo3 <Gwozdo3@interia.pl>
2016-11-07 02:19:17 +0100

    updated tetromino creation, tratata

--- src/TetrisGA/Tetromino.java
+++ src/TetrisGA/Tetromino.java
@@ -8,7 +8,7 @@ package TetrisGA;
  *  -printTetArray  - prints tetromino in console
  *  -rotateTetromino - rotates teromino array
  *  -getTetArray - returns tetArray
- *  -createTet - generates new Tetromino
+ *  -createTet - generates new Tetr..omino
  *
  */
 import java.util.Random;
